e8fce2f97cdc55d93166af3ae50590b8696d8755
Luiz Claudio <lcluizclaudio96@gmail.com>
2022-08-24 16:24:46 -0300

    setup jest / react native testing library

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,19 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      [
-        "module:react-native-dotenv",
-        {
-          envName: "APP_ENV",
-          moduleName: "react-native-dotenv",
-          path: ".env",
-          safe: false,
-          allowUndefined: true,
-          verbose: false,
-        },
-      ],
-    ],
-  };
+	api.cache(true);
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			[
+				'module:react-native-dotenv',
+				{
+					envName: 'APP_ENV',
+					moduleName: 'react-native-dotenv',
+					path: '.env',
+					safe: false,
+					allowUndefined: true,
+					verbose: false,
+				},
+			],
+		],
+	};
 };
